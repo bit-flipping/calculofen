59bf89c7ca46e6e87e4067922b21d9d38590c5ca
correajr <rcsjunior1976@gmail.com>
2025-02-05 15:54:02 -0300

    inserido arquivo 2

--- calcfen/views.py
+++ calcfen/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 import cv2
 import easyocr
 
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+
+#################################
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'fen/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'fen/simple_upload.html')
+
+################################
+
 
 def home(request):
     return render(request, 'fen/home.html')
